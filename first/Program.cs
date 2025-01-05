a2a68576e3a2000c64514af462534acea9fe2ab0
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-05 19:09:15 +0500

    new

---
+++ first/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace first {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Hello world!"); // Мы пишем данные в 1 строку(Line)
+            Console.Write("Привет!");
+            Console.Write("Привет!\n"); // Выведется Привет!Привет! так как нет переноса на новую строку. (\n для след примера, тип на сл строку перенос)
+            Console.Write("Новаястрока!");
+
+            Console.ReadKey(); // Ожидаем ввода хотя бы одного символа 
+            Console.ReadLine(); // Вводим данные с клавиатуры до нажатия Enter
+        }
+    }
+}
---
+++ th4/myfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace th4 {
+    class myfile {
+        static void Main() {
+            Console.Write("Введите нецелое число через запятую(,): ");
+
+            //user_input = Convert.ToDouble(Console.ReadLine()); // Чтобы исправить ошибку в этой строке, можно просто float в предыдущей поменять на double
+            float user_input = float.Parse(Console.ReadLine()); // Второй способ исправления ошибки без перевода в double, только в консоли надо
+                                                                // использовать НЕ ТОЧКУ, а ЗАПЯТУЮ
+            float result = user_input + 10.1f; // Можно и без f, но как-то логичнее выглядит
+            result = user_input - 10.1f;
+            //result = user_input * 12.12f;
+            //result = user_input / 1255.12f;
+            //result = user_input % 10.2f;
+
+            int a = 5, b = 6;
+            int res = a + b;
+            res += 12;
+
+
+
+
+
+            Console.WriteLine("Переменная: " + result);
+        }
+    }
+}
