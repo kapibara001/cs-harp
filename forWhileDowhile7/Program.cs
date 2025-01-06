165eff78bba818e1252a2c07137570bb66cb4f2a
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-06 17:44:14 +0500

    new

---
+++ forWhileDowhile7/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace forwhile {
+    class myclass {
+        static void Main() {
+            
+        }
+    }
+}
--- switchCase6/mymain.cs
+++ switchCase6/mymain.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+namespace code {
+    class Switchcase {
+        static void Main() {
+            Console.Write("Укажите какое-то число: ");
+            short num = Convert.ToInt16(Console.ReadLine());
+
+        // Switch() {case n: ... break; case m: ... break;}
+            switch(num) {
+                case 2:
+                    Console.WriteLine("Мало");
+                    break;
+                case 3:
+                case 15:
+                case 23:
+                    Console.WriteLine("Понял!");
+                    break;
+                default:
+                    Console.WriteLine("Число не распознано!");
+                    break;
+            }
+        }
+    }
+}
