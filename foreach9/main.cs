a2e7c071a9b27dfa396206ad2050dab0024c1595
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-06 20:23:56 +0500

    9foreach

---
+++ foreach9/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace names {
+    class Myclass {
+        static void Main() {
+        // Цикл foreach макимально подходит для работы с массивами
+            short[] nums = {5, 6, 7, 8, 9, 4, 1};
+            
+            foreach(short el in nums) {
+                Console.WriteLine("El: {0}", el);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
