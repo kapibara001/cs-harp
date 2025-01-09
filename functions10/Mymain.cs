ac18b3f4e0c10426f983d36d1b43556ff504ac42
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-09 11:36:14 +0500

    files

--- functions10/Mymain.cs
+++ functions10/Mymain.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Security.Cryptography;
 
 namespace project {
     class Prog {
@@ -17,21 +15,60 @@ namespace project {
                 Console.WriteLine(el);
             */
 
-            Print(); // Вызов функции print()
-            Print();
-            Print();
-            string word = "Падлыова";
+            Print(); // Вызов функции print() если бы не было обязательного параметра
+
+            string word = "Павел Дуров";
             ushort age = 11 ;
-            PrintWord(word, age);
+            PrintWord(word, age); // Вызов функции с обязательными параметрами
+
+            float res = MathematicOperations(12.125125f, 125533.12412f);
+            Printres(res.ToString());
+
+
+            byte[] nums1 = {5, 7, 1, 3, 5}; // Практическое задание
+            byte res1= ArraySum(nums1);
+            Printres(res1.ToString());
+
+            float[] nums2 = {5.6f, 7.412f, 1, 12.46f, 24, 6, 1};
+            float res2 = ArraySum(nums2);
+            Printres(res2.ToString());
         }
 
-        public static void Print() {        // public - за счет него мы можем обращаться к этой функции(методу) из всех классов. 
-            Console.WriteLine("Hello!");    // static - он говорит о том, что мы можем обратиться у функции просто по ее имени 
-        }                                   // void - Признак того, что функция ничего не должна возвращать
+        public static void Printres(string word) {        // public - за счет него мы можем обращаться к этой функции(методу) из всех классов. 
+            Console.WriteLine(word);                      // static - он говорит о том, что мы можем обратиться у функции просто по ее имени 
+        }                                                 // void - Признак того, что функция ничего не должна возвращать
+        
+        public static void Print() {
+            Console.WriteLine("Hello!");
+        }
+
             
         public static void PrintWord(string word, ushort age) { // Указанные в скобках данные функция как бы ожидает для дальнейшей работы с ними. 
                                                                 // В случае, если данные требуются, но они не указаны - будет ошибка
             Console.WriteLine("Привет! Ваше слово: {0} и вам {1} лет.", word, age);
         }
+
+        public static float MathematicOperations(float x, float y) {
+            return x + y;
+        }
+
+
+
+    // Практическое задание. Все переменные, связанные с этой функцией, находятся в Main()
+        public static byte ArraySum(byte[] arr) { // Также можно ПЕРЕЗАГРУЖАТЬ функции, то есть создавать ту же функцию с тем же именем, но менять тип данных
+            byte sum = 0;                         // Наример в этой функции обязателен тип byte, но можно сделать эту же функцию, но с типом float к примеру.
+            foreach (byte el in arr) {
+                sum += el;
+            }
+            return sum;
+        } 
+
+         public static float ArraySum(float[] arr) {  // Перезагрузка функции.
+            float sum = 0;                         
+            foreach (float el in arr) {
+                sum += el;
+            }
+            return sum;
+        } 
     }
 }
---
+++ workWithFiles11/WorkWithFiles.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace files {
+    class WorkWithFiles {
+        public static void Main() {
+        // СТРОКИ
+            string word = "Hello"; // По факту - string - это просто массив элементов типа char
+            //Console.WriteLine(word[1]); // Подтверждение того, что string - массив типа char
+            word += "!";
+            Console.WriteLine(word);
+
+            
+        // Некоторые функции работы со строками
+            //Console.WriteLine(word.Length); // .Length - измерение длины
+            //Console.WriteLine(String.Concat(word, "!!")); // .Concat("к чему", "что") Добавление подстрок к строке через String.Concat
+            //Console.WriteLine(String.Compare(word, "Hello!!")); // .Compare("что", "с чем") - Сравнение двух строк. Если выдает 0 - строки равны. 
+                                                                // Если 1 - то в сравниваемой строке не хватает 1 символа. Если -1 - то на 1 символ больше.
+
+            string people = "Alex,Bob,John";
+            string[] names = people.Split(','); // (.Solit("по какому символу")) Разбиение по знаку ",". В курсе эта строка выглядит так: string[] names = people.Split(new char[] {','});
+            foreach(string name in names) {
+                //Console.WriteLine("Имя - {0}", name);
+            }
+            people = String.Join(" ", names); // Разделили строку по запятой в 20 строке и соединиили (.Join("как соединить", что соединить)) через пробел в этой строке
+            Console.WriteLine(people);
+
+            word += "!       ";
+            Console.WriteLine(word.Trim()); //.Trim() - Удаление пробелов, которые мы добавили в 27 строке
+            Console.WriteLine(word.Substring(2)); // Обрезание строки word с 0 по 2 индекс (0, 1)
+            Console.WriteLine(word.Substring(0, word.Length-1)); // Обрезание только последнего элемента. 0 - начинаем с 0 элемента, ничего не удаляем
+                                                                 // word.Length-1 - обрезание последнего символа
+        /* 
+            .StartsWith() - проверка на то, что строка начинается с символов в скобках (True, False)
+            .ToLower(), .ToUpper() - приведение к нижнеему и верхнему регистру соответственно
+        */
+
+            
+        // РАБОТА С ФАЙЛАМИ
+        // Для работы с ними надо подключить библеотеку System.IO;
+
+            using(FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate)) { //Изоляция кода с помощью using()
+                // класс FileSteam позволяет открыть файл для чтения или записи. Выделаем память, задаем ему имя и метод открытия(создание, открытие или создание и тп)
+                Console.Write("Введите текст, который будет записан в файл: ");
+                string usertext = Console.ReadLine();
+                // Далее необходимо полученный текст преобразовать в байты(0 и 1) для передачи в файл
+                byte[] array = System.Text.Encoding.Default.GetBytes(usertext); // преобразование текста в массив байтов
+                
+                stream.Write(array, 0, array.Length); // Запись в файл через Write(stream уже создан)
+                                                      // array - тут наш текст, 0 - отступ(сколкьо пропускаем от начала), array.Length - до куда
+            }
+
+            using(FileStream stream1 = File.OpenRead("info.txt")) { // Открытие файла для получения из него инфы
+                byte[] array = new byte[stream1.Length];
+                stream1.Read(array, 0, array.Length);
+
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine(textFromFile);
+            }   
+        }
+    }
+}
