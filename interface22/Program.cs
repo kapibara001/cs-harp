d8698050daccd00b5a958415ebaf471d6353e655
kapibara001 <136171131+kapibara001@users.noreply.github.com>
2025-01-22 15:18:10 +0500

    interfaces22

---
+++ interface22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace interface22 {
+    class Program {
+        static void Main() {
+            //Car Hundai = new Car("Hetchback", 20000, 1500, 3000); // Выдает ошибку т.к на основе абстрактоного класса нельзя создать объекты
+            //Hundai.printCarValues();
+
+            Jeep RamTRX = new Jeep("Jeep", 45000, 4500, 5, 2.15f);
+            RamTRX.printCarValues();
+            RamTRX.Hello();
+            RamTRX.Jumpa(); // Использовал подключенный к carMain.cs интерфейс
+            RamTRX.Jumped(); // Использовал подключенный к JeepCar.cs интерфейс
+
+            Bot bot1 = new Bot("Grand", 400000, 1200, 1);
+            bot1.printCarValues();
+
+            Bot bot2 = new Bot("GTA", 1000, 1500, 2);
+            bot1.printCarValues();
+
+            Bot bot3 = new Bot("Hetchback", 20000, 1500, 3000);
+            bot1.printCarValues();            
+        }
+    }
+}
